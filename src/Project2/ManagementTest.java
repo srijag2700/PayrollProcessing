688e6e3a0b0ec543eec2d18b4de7103d4c7b0206
Srija Gottiparthi <srija.gottiparthi@gmail.com>
2021-02-22 20:16:28 -0500

    junit done

--- src/Project2/ManagementTest.java
+++ src/Project2/ManagementTest.java
@@ -1,6 +1,6 @@
 package Project2;
 
-import org.junit.Assert.*;
+import static org.junit.Assert.*;
 import org.junit.Test;
 import java.text.DecimalFormat;
 
@@ -8,38 +8,18 @@ public class ManagementTest {
     DecimalFormat df = new DecimalFormat("$#,###,###,##0.00");
 
     @Test
-    public void testToString() {
-        Profile testEmpProf1 = new Profile("Doe,Jane", "CS", new Date("1/1/2020"));
-        Management testMgmt1 = new Management(testEmpProf1, 95000, 1);
-        assertEquals("Doe,Jane::CS::1/1/2020::Payment $3,653.85::FULL TIME::Annual Salary $95,000.00::" +
-                "Manager Compensation $192.31", testMgmt1.toString());
-        Profile testEmpProf2 = new Profile("Gottiparthi,Srija", "ITI", new Date("2/1/2012"));
-        Management testMgmt2 = new Management(testEmpProf2, 90000, 2);
-        assertEquals("Gottiparthi, Srija::ITI::2/1/2012::Payment $3,461.54::FULL TIME::Annual Salary $90,000.00::" +
-                "DepartmentHead Compensation $365.38", testMgmt2.toString());
-        Profile testEmpProf3 = new Profile("Nguyen,Catherine", "ECE", new Date("2/27/2014"));
-        Management testMgmt3 = new Management(testEmpProf3, 80000, 3);
-        assertEquals("Nguyen,Catherine::ECE::2/27/2014::Payment $3,076.92::FULL TIME::Annual Salary $80,000.00::" +
-                "Director Compensation $461.54", testMgmt3.toString());
-    }
-
-    @Test
-    public void testEquals() {
-        Profile testEmpProf1 = new Profile("Doe,Jane", "CS", new Date("1/1/2020"));
-        Management testMgmt1 = new Management(testEmpProf1, 95000, 1);
-        Profile testEmpProf2 = new Profile("Doe,Jane", "CS", new Date("1/1/2020"));
-        Management testMgmt2 = new Management(testEmpProf2, 95000, 1);
-        assertEquals(true, testMgmt1.equals(testMgmt2));
-        Profile testEmpProf3 = new Profile("Doe,Jane", "CS", new Date("1/1/2020"));
-        Management testMgmt3 = new Management(testEmpProf3, 95000, 3);
-        assertEquals(false, testMgmt1.equals(testMgmt3));
-    }
-
-    @Test
     public void testCalculatePayment() {
         Profile testEmpProf1 = new Profile("Doe,Jane", "CS", new Date("1/1/2020"));
-        Management testMgmt1 = new Management(testEmpProf1, 95000, "Manager");
+        Management testMgmt1 = new Management(testEmpProf1, 95000, 1);
         testMgmt1.calculatePayment();
-        assertEquals(3846.15, testMgmt1.getPayment());
+        assertEquals(3846.15, testMgmt1.getPayment()); // test case #1, calculating payment of manager
+        Profile testEmpProf2 = new Profile("Gottiparthi,Srija", "ITI", new Date("2/1/2012"));
+        Management testMgmt2 = new Management(testEmpProf2, 90000, 2);
+        testMgmt2.calculatePayment();
+        assertEquals(3826.92, testMgmt2.getPayment()); // test case #2, calculating payment of department head
+        Profile testEmpProf3 = new Profile("Nguyen,Catherine", "ECE", new Date("2/27/2014"));
+        Management testMgmt3 = new Management(testEmpProf3, 80000, 3);
+        testMgmt3.calculatePayment();
+        assertEquals(3538.46, testMgmt3.getPayment()); // test case #3, calculating payment of director
     }
 }
