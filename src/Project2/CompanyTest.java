86a8ef4e3581f672f66944a7eccb7e4a87a8c7b4
Srija Gottiparthi <9939776+srijag2700@users.noreply.github.com>
2021-02-22 19:41:05 -0500

    Add files via upload

---
+++ src/Project2/CompanyTest.java
@@ -0,0 +1,51 @@
+package Project2;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.*;
+
+public class CompanyTest {
+    @Test
+    public void testFind() {
+
+    }
+
+    @Test
+    public void testGrow() {
+
+    }
+
+    @Test
+    public void testAdd() {
+
+    }
+
+    @Test
+    public void testRemove() {
+
+    }
+
+    @Test
+    public void testSetHours() {
+
+    }
+
+    @Test
+    public void testProcessPayments() {
+
+    }
+
+    @Test
+    public void testPrint() {
+
+    }
+
+    @Test
+    public void testPrintByDepartment() {
+
+    }
+
+    @Test
+    public void testPrintByDate() {
+
+    }
+}
