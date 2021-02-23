213956c5416285043400e17a907080ffc8f67f0c
Catherine Nguyen <catherinevn.11@gmail.com>
2021-02-22 19:50:06 -0500

    junit

--- src/Project2/ManagementTest.java
+++ src/Project2/ManagementTest.java
@@ -1,7 +1,7 @@
 package Project2;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Assert.*;
+import org.junit.Test;
 import java.text.DecimalFormat;
 
 public class ManagementTest {
@@ -10,15 +10,15 @@ public class ManagementTest {
     @Test
     public void testToString() {
         Profile testEmpProf1 = new Profile("Doe,Jane", "CS", new Date("1/1/2020"));
-        Management testMgmt1 = new Management(testEmpProf1, 95000, "Manager");
+        Management testMgmt1 = new Management(testEmpProf1, 95000, 1);
         assertEquals("Doe,Jane::CS::1/1/2020::Payment $3,653.85::FULL TIME::Annual Salary $95,000.00::" +
                 "Manager Compensation $192.31", testMgmt1.toString());
         Profile testEmpProf2 = new Profile("Gottiparthi,Srija", "ITI", new Date("2/1/2012"));
-        Management testMgmt2 = new Management(testEmpProf2, 90000, "DepartmentHead");
+        Management testMgmt2 = new Management(testEmpProf2, 90000, 2);
         assertEquals("Gottiparthi, Srija::ITI::2/1/2012::Payment $3,461.54::FULL TIME::Annual Salary $90,000.00::" +
                 "DepartmentHead Compensation $365.38", testMgmt2.toString());
         Profile testEmpProf3 = new Profile("Nguyen,Catherine", "ECE", new Date("2/27/2014"));
-        Management testMgmt3 = new Management(testEmpProf3, 80000, "Director");
+        Management testMgmt3 = new Management(testEmpProf3, 80000, 3);
         assertEquals("Nguyen,Catherine::ECE::2/27/2014::Payment $3,076.92::FULL TIME::Annual Salary $80,000.00::" +
                 "Director Compensation $461.54", testMgmt3.toString());
     }
@@ -26,12 +26,12 @@ public class ManagementTest {
     @Test
     public void testEquals() {
         Profile testEmpProf1 = new Profile("Doe,Jane", "CS", new Date("1/1/2020"));
-        Management testMgmt1 = new Management(testEmpProf1, 95000, "Manager");
+        Management testMgmt1 = new Management(testEmpProf1, 95000, 1);
         Profile testEmpProf2 = new Profile("Doe,Jane", "CS", new Date("1/1/2020"));
-        Management testMgmt2 = new Management(testEmpProf2, 95000, "Manager");
+        Management testMgmt2 = new Management(testEmpProf2, 95000, 1);
         assertEquals(true, testMgmt1.equals(testMgmt2));
         Profile testEmpProf3 = new Profile("Doe,Jane", "CS", new Date("1/1/2020"));
-        Management testMgmt3 = new Management(testEmpProf3, 95000, "Director");
+        Management testMgmt3 = new Management(testEmpProf3, 95000, 3);
         assertEquals(false, testMgmt1.equals(testMgmt3));
     }
 
